58cbe206551b116b8055db9d9f7f0bf6aa69b7eb
plex <plex@plex-server.attlocal.net>
2022-03-04 19:31:08 -0800

    Updated Analysis Confirming 4k Eligible

--- algomint_investigation.py
+++ algomint_investigation.py
@@ -1,17 +1,29 @@
 import json
 import requests
 
-algomint_address = 'ETGSQKACKC56JWGMDAEP5S2JVQWRKTQUVKCZTMPNUGZLDVCWPY63LSI3H4'
-goBTC = 386192725
-goETH = 386195940
-
-response = requests.get(f'https://api.algoexplorer.io/idx2/v2/accounts/{algomint_address}/transactions')
-transaction_dict = response.json()
-
+transaction_dict = {}
 receiver_set = set()
 goBTC_set = set()
 goETH_set = set()
 
+#Algomint Addess and Asset ID's
+algomint_address = 'ETGSQKACKC56JWGMDAEP5S2JVQWRKTQUVKCZTMPNUGZLDVCWPY63LSI3H4'
+goBTC = 386192725
+goETH = 386195940
+
+# For Paginated API Call Results. Max Limit 1,000 Transactions
+next_token = ''
+
+#Iterating over 10 pages of transactions
+for i in range(9):
+    response = requests.get(f'https://api.algoexplorer.io/idx2/v2/accounts/{algomint_address}/transactions?limit=1000&next={next_token}')
+    if transaction_dict:
+        transaction_dict['transactions'].extend(response.json()['transactions'])
+    else:
+        transaction_dict = response.json()
+    next_token = response.json()['next-token']
+
+#Identifying asset transfer transactions sent by Algomint and categorizing into goBTC/goETH sets
 for tx in transaction_dict['transactions']:
     if 'asset-transfer-transaction' in tx:
         receiver = tx['asset-transfer-transaction']['receiver']
@@ -27,4 +39,14 @@ print(f'Total Mint Addresses From Algomint: {len(receiver_set)}')
 print(f'Total goBTC Only Mint Addresses: {len((goBTC_set^goETH_set)&goBTC_set)}')
 print(f'Total goETH Only Mint Addresses: {len((goETH_set^goBTC_set)&goETH_set)}')
 print(f'Total Double Mint Addresses: {len(goBTC_set.intersection(goETH_set))}')
-print(receiver_set)
+
+#Dump data to files
+with open('mint_addresses.txt', 'w') as file:
+    for item in receiver_set:
+        file.write(item + '\n')
+    print('Mint Receiving Addresses Dumped as mint_addresses.txt')
+
+with open('mint_transactions.json', 'w') as file:
+    json.dump(transaction_dict, file)
+    print('Transactions Dumped as mint_transactions.json')
+
